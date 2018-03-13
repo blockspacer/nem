8c01683de7b4aa80719016e4a5807da44f748417
lye <lye@>
2018-03-13 14:29:33 -0700

    Scaffold out nem-routerd.
    Let's start integrating TLS.

---
+++ nem-routerd/build.sh
@@ -0,0 +1,65 @@
+#!/bin/sh -e
+
+cd `dirname $0`
+
+CC=clang37
+SOURCE_FILES=src/*.c
+
+if `which ccache > /dev/null 2>&1` ; then
+	CC="ccache $CC"
+fi
+
+LIBS="
+	-L/usr/local/lib
+	-lexecinfo
+	-licuuc
+	-lcxxrt
+	-lz
+	-lgeom
+	/home/lye/code/libtoml2/bin/libtoml2.a
+"
+
+BUILD_FLAGS="
+	-g
+	-Wall -Werror -Wno-unused-function
+	-fno-omit-frame-pointer
+	-fsanitize=undefined
+	-ferror-limit=5
+	-std=c11
+	-isystem/usr/local/include
+	-isystem/home/lye/code/libtoml2/inc
+	-Iinc
+	-I../libnem/inc
+"
+
+mkdir -p bin
+mkdir -p obj
+
+OBJ_FILES=
+
+for f in src/* ; do
+	OBJ_FILE=`echo $f | sed -e 's#^src#obj#' -e 's#.c$#\.o#'`
+	$CC -c -o $OBJ_FILE $BUILD_FLAGS $f
+
+	if [ $f != 'src/main.c' ] ; then 
+		OBJ_FILES="$OBJ_FILES $OBJ_FILE"
+	fi
+done
+
+$CC \
+	$BUILD_FLAGS \
+	$OBJ_FILES \
+	obj/main.o \
+	-o ./bin/nem-routerd \
+	$LIBS \
+	../libnem/bin/libnem.a
+
+$CC \
+	$BUILD_FLAGS \
+	$OBJ_FILES \
+	test/*.c \
+	-o ./bin/routerd.test \
+	$LIBS -lcheck \
+	../libnem/bin/libnem.a
+
+./bin/routerd.test
