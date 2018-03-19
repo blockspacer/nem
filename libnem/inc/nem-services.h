889b257a2ee407971b4190c57767b75b3f29414e
lye <lye@>
2018-03-18 23:28:18 -0700

    Start fleshing out some initial command ids.
    These are all centralized in a single file for now -- at some point
    the proto version might need to be bumped or something to make this
    more extensible. For the Go implementation of the service framework
    there's a total duplication of all of this, which is kind of a mess.

--- libnem/inc/nem-services.h
+++ libnem/inc/nem-services.h
@@ -1,8 +1,25 @@
 #pragma once
 
+// This file describes service/command ids. These are basically hardcoded
+// everywhere rather than passing strings. It might make more sense to pass
+// strings, but those can be stuffed into the headers under a specified 
+// "use strings instead" service. Anyway it's easier parsing/dispatching on
+// the C side if these are just integers.
+
+// This is the master list of services that can be provided by daemons. These
+// are stuffed into NEM_pmsg_t.service_id and indicate which service type should
+// be targetted.
 static const uint16_t 
 	NEM_svcid_daemon = 1,
-	NEM_svcid_host = 2,
+	NEM_svcid_host   = 2,
 	NEM_svcid_router = 3;
 
+static const uint16_t
+	NEM_cmdid_daemon_name   = 1,
+	NEM_cmdid_daemon_getcfg = 2,
+	NEM_cmdid_daemon_setcfg = 3,
+	NEM_cmdid_daemon_stop   = 4;
+
 const char* NEM_svcid_to_string(uint16_t svcid);
+bool NEM_svcid_is_routable(uint16_t svcid);
+const char* NEM_cmdid_to_string(uint16_t svcid, uint16_t cmdid);
--- libnem/src/services.c
+++ libnem/src/services.c
@@ -1,14 +1,40 @@
 #include "nem.h"
 
-static const struct {
-	uint16_t svcid;
+typedef struct {
+	uint16_t    cmdid;
 	const char *name;
 }
-svc_id_map[] = {
-	{ NEM_svcid_daemon, "daemon" },
-	{ NEM_svcid_host,   "host"   },
-	{ NEM_svcid_router, "router" },
+cmd_data_t;
+
+static const cmd_data_t daemon_cmds[] = {
+	{ NEM_cmdid_daemon_name,   "name"   },
+	{ NEM_cmdid_daemon_getcfg, "getcfg" },
+	{ NEM_cmdid_daemon_setcfg, "setcfg" },
+	{ NEM_cmdid_daemon_stop,   "stop"   },
 };
+
+static const cmd_data_t host_cmds[] = {
+};
+
+static const cmd_data_t router_cmds[] = {
+};
+
+#define CMD_DEF(name, routable) {\
+	NEM_svcid_##name, #name, routable, name##_cmds, NEM_ARRSIZE(name##_cmds) \
+}
+static const struct {
+	uint16_t          svcid;
+	const char       *name;
+	bool              routable;
+	const cmd_data_t *commands;
+	size_t            num_commands;
+}
+svc_id_map[] = {
+	CMD_DEF(daemon, false),
+	CMD_DEF(host, false),
+	CMD_DEF(router, false),
+};
+#undef CMD_DEF
 
 const char*
 NEM_svcid_to_string(uint16_t svcid)
@@ -16,6 +42,36 @@ NEM_svcid_to_string(uint16_t svcid)
 	for (size_t i = 0; i < NEM_ARRSIZE(svc_id_map); i += 1) {
 		if (svc_id_map[i].svcid == svcid) {
 			return svc_id_map[i].name;
+		}
+	}
+
+	return "unknown";
+}
+
+bool
+NEM_svcid_is_routable(uint16_t svcid)
+{
+	for (size_t i = 0; i < NEM_ARRSIZE(svc_id_map); i += 1) {
+		if (svc_id_map[i].svcid == svcid) {
+			return svc_id_map[i].routable;
+		}
+	}
+
+	return false;
+}
+
+const char*
+NEM_cmdid_to_string(uint16_t svcid, uint16_t cmdid)
+{
+	for (size_t i = 0; i < NEM_ARRSIZE(svc_id_map); i += 1) {
+		if (svc_id_map[i].svcid == svcid) {
+			const cmd_data_t *cmds = svc_id_map[i].commands;
+			for (size_t j = 0; j < svc_id_map[i].num_commands; i += 1) {
+				if (cmds[j].cmdid == cmdid) {
+					return cmds[j].name;
+				}
+			}
+			break;
 		}
 	}
 
