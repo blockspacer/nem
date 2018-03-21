4f6b073e0e1baa999a9a6f9a2c253616666bd4db
lye <lye@>
2018-03-21 00:52:19 -0700

    NEM_c_routerd: automatically restart the child if it crashes.

--- nem-rootd/src/c_routerd.c
+++ nem-rootd/src/c_routerd.c
@@ -9,12 +9,60 @@ static NEM_child_t        child;
 static NEM_rootd_svcmgr_t svcmgr;
 static NEM_rootd_txnmgr_t txnmgr;
 static bool               is_running = false;
+static bool               want_running = true;
+
+static NEM_err_t routerd_start(NEM_app_t *app);
+
+static void
+routerd_restart_cb(NEM_thunk1_t *thunk, void *varg)
+{
+	if (!want_running) {
+		printf("c-routerd: nevermind, leave it dead\n");
+		return;
+	}
+	if (is_running) {
+		printf("c-routerd: child resurrected itself?!\n");
+		return;
+	}
+
+	NEM_app_t *app = NEM_thunk1_ptr(thunk);
+	NEM_err_t err = routerd_start(app);
+	if (!NEM_err_ok(err)) {
+		if (NEM_rootd_verbose()) {
+			printf(
+				"c-routerd: couldn't start child: %s\n",
+				NEM_err_string(err)
+			);
+		}
+
+		NEM_app_after(app, 1000, NEM_thunk1_new_ptr(
+			&routerd_restart_cb,
+			app
+		));
+	}
+}
 
 static void
 routerd_restart(NEM_thunk1_t *thunk, void *varg)
 {
-	// XXX: Really want some retry semantics here.
-	NEM_rootd_shutdown();
+	is_running = false;
+
+	if (!want_running) {
+		if (NEM_rootd_verbose()) {
+			printf("c-routerd: child died? good riddance\n");
+		}
+		return;
+	}
+
+	if (NEM_rootd_verbose()) {
+		printf("c-routerd: child died? restarting in 10ms\n");
+	}
+
+	NEM_app_t *app = NEM_thunk1_ptr(thunk);
+	NEM_app_after(app, 10, NEM_thunk1_new_ptr(
+		&routerd_restart_cb,
+		app
+	));
 }
 
 static void
@@ -101,6 +149,7 @@ static bool
 try_shutdown()
 {
 	printf("c-routerd: try-shutdown\n");
+	want_running = false;
 	return true;
 }
 
