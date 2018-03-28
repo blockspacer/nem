17622b35f9d4f06930fb39fe98611c0c0421c5ed
lye <lye@>
2018-03-28 16:33:43 -0700

    nem-rootd: c-lockfile: fix a bug in the path generation; unlink file.

--- nem-rootd/src/c-lockfile.c
+++ nem-rootd/src/c-lockfile.c
@@ -4,6 +4,7 @@
 
 static int lock_fd;
 static const char *lock_name = "lockfile";
+static char *lock_path = NULL;
 
 static NEM_err_t
 setup(NEM_app_t *app)
@@ -12,13 +13,11 @@ setup(NEM_app_t *app)
 		printf("c-lockfile: setup\n");
 	}
 
-	char *lock_path;
-	if (0 > asprintf(&lock_path, NEM_rootd_jail_root(), lock_name)) {
+	if (0 > asprintf(&lock_path, "%s/%s", NEM_rootd_jail_root(), lock_name)) {
 		return NEM_err_errno();
 	}
 
 	lock_fd = open(lock_path, O_CREAT|O_NOFOLLOW|O_CLOEXEC, 0600);
-	free(lock_path);
 	if (0 > lock_fd) {
 		return NEM_err_errno();
 	}
@@ -30,6 +29,10 @@ setup(NEM_app_t *app)
 		}
 		close(lock_fd);
 		return err;
+	}
+
+	if (NEM_rootd_verbose()) {
+		printf("c-lockfile: locked '%s'\n", lock_path);
 	}
 
 	return NEM_err_none;
@@ -44,6 +47,16 @@ teardown()
 
 	flock(lock_fd, LOCK_UN);
 	close(lock_fd);
+	if (0 != unlink(lock_path)) {
+		if (NEM_rootd_verbose()) {
+			printf(
+				"c-lockfile: unable to unlink '%s': '%s'\n",
+				lock_path,
+				NEM_err_string(NEM_err_errno())
+			);
+		}
+	}
+	free(lock_path);
 }
 
 const NEM_rootd_comp_t NEM_rootd_c_lockfile = {
